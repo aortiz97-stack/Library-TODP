090f5b0d4551c502ecdac189230514ff6b326b98
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-26 16:10:39 -0600

    Add feature for adding book to display

--- script.js
+++ script.js
@@ -13,14 +13,17 @@ function Book(title, author, pagesNumber, read) {
 
 const addBookButton = document.querySelector('.add-book');
 const formPopUp = document.querySelector('.form-popup');
+const exitFormButton = document.querySelector('form .exit');
+const inputs = document.querySelectorAll('form input');
+const submitButton = document.querySelector('form button[type="submit"]');
+const mainGrid = document.querySelector('.main');
+
 addBookButton.addEventListener('click', () => {
   formPopUp.style.display = 'flex';
 });
 
-const exitFormButton = document.querySelector('form .exit');
 exitFormButton.addEventListener('click', () => {
   formPopUp.style.display = 'none';
-  const inputs = document.querySelectorAll("form input");
   inputs.forEach((input) => {
     if (input.type !== 'checkbox') {
       input.value = '';
@@ -28,4 +31,47 @@ exitFormButton.addEventListener('click', () => {
       input.checked = false;
     }
   });
+});
+
+console.log(submitButton);
+submitButton.addEventListener('click', (e) => {
+    console.log("enterred");
+  e.preventDefault();
+  const inputValues = [];
+  inputs.forEach((input) => {
+    inputValues.push(input.value);
+  });
+
+  const newBook = new Book(
+    inputValues[0],
+    inputValues[1],
+    inputValues[2],
+    inputValues[3],
+  );
+
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+  const bookContainerDiv = document.createElement('div')
+  bookContainerDiv.classList.add('book-container');
+
+  function addHTML(elementType, bookProperty) {
+    const HTML = document.createElement(elementType);
+    HTML.textContent = bookProperty;
+    bookDiv.appendChild(HTML);
+  }
+
+  addHTML('h2', newBook.title);
+  addHTML('h3', newBook.author);
+  addHTML('h3', newBook.pagesNumber);
+  addHTML('h3', newBook.read);
+
+  const formSubmitButton = document.createElement('button');
+  formSubmitButton.setAttribute('type', 'submit');
+  bookDiv.appendChild(formSubmitButton);
+  const formRemoveButton = document.createElement('button');
+  formRemoveButton.classList.add('remove');
+  bookDiv.appendChild(formRemoveButton);
+
+  bookContainerDiv.appendChild(bookDiv);
+  mainGrid.appendChild(bookContainerDiv);
 });
